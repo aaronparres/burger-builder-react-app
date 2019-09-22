b7ebacf9e660ecbe178d31f4d8fdecd237c49a36
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-09-22 04:55:30 +0200

    Added logic on Burger.js to dynamically create the ingredients from the state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,24 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
-const Burger = ( props ) => {
+const Burger = (props) => {
+
+    // console.log(Object.keys(props.ingredients));
+    // console.log(props.ingredients);
+    // console.log(props.ingredients["meat"]);
+    // console.log([...Array(props.ingredients["meat"])]); // returns an array with two fields undefined
+
+    const transformIngredients = Object.keys(props.ingredients).map(ingType => {
+        return [...Array(props.ingredients[ingType])].map((_, i) => {
+            return <BurgerIngredient key={ingType + i} type={ingType} />; // key={salad1}
+        });
+    });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformIngredients}
             <BurgerIngredient type="bread-bottom" />
-
         </div>
     );
 };
