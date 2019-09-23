948834a24323d0051d5603c5f1c5762739c358be
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-09-24 01:20:45 +0200

    Added Toolbar with Logo component and ajust the height of the Layout to 72px to create space under the bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = ( props ) => (
     <Fragment>
+        <Toolbar />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children} {/* props.children --> <BurgerBuilder /> */}
