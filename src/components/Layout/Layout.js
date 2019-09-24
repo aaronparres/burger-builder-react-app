892f69392dc1638e854e0c69e5efae8702718600
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-09-24 02:36:12 +0200

    Added Navigation Items, SideDrawer with a logo and navigation items, it opens and closes and added burger icon to the toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = ( props ) => (
-    <Fragment>
-        <Toolbar />
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children} {/* props.children --> <BurgerBuilder /> */}
-        </main>
-    </Fragment>
-);
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer } 
+        });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+                <div>Toolbar, SideDrawer, Backdrop</div>
+                <main className={classes.Content}>
+                    {this.props.children} {/* props.children --> <BurgerBuilder /> */}
+                </main>
+            </Fragment >
+        );
+
+    }
+}
+
 export default Layout;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,9 +3,9 @@ import React from 'react';
 import logo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css';
 
-const Logo = () => {
+const Logo = (props) => {
     return (
-        <div className={classes.Logo}>
+        <div className={classes.Logo} style={{height: props.height}}>
             <img src={logo} alt="burger-logo" />
         </div>
     );
