9892973c933f94229e51e430e8d6a1a7ac7532cf
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-09-24 02:58:11 +0200

    Improve PopUp component, it only updates when show property changes

--- src/components/UI/PopUp/PopUp.js
+++ src/components/UI/PopUp/PopUp.js
@@ -1,22 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import classes from './PopUp.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const PopUp = (props) => {
-    return (
-        <Fragment>
-            <Backdrop show={props.show} clicked={props.popUpClosed} />
-            <div
-                className={classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                {props.children} {/* --> <OrderSummary />*/}
-            </div>
-        </Fragment>
-    );
-};
+class PopUp extends Component {
+
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Backdrop show={this.props.show} clicked={this.props.popUpClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children} {/* --> <OrderSummary />*/}
+                </div>
+            </Fragment>
+        );
+    }
+}
 
 export default PopUp;
