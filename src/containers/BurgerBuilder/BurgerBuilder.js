212e5c1b96868018a60cb9cc130a760242f89587
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-10-15 18:06:23 +0200

    Base routing set-up (BurgerBuilder and Checkout)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,31 +86,33 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Aarón Parres',
-                addres: {
-                    street: 'La tercia',
-                    zipCode: '00000',
-                    country: 'Españita'
-                },
-                email: 'jate@mate.com'
-            },
-            deliveryMethod: 'rapidico'
-        };
+        // this.setState({ loading: true });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Aarón Parres',
+        //         addres: {
+        //             street: 'La tercia',
+        //             zipCode: '00000',
+        //             country: 'Españita'
+        //         },
+        //         email: 'jate@mate.com'
+        //     },
+        //     deliveryMethod: 'rapidico'
+        // };
 
-        Axios.post('/orders.json', order)
-            .then(response => {
-                console.log(response);
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                console.log(error);
-                this.setState({ loading: false, purchasing: false });
-            });
+        // Axios.post('/orders.json', order)
+        //     .then(response => {
+        //         console.log(response);
+        //         this.setState({ loading: false, purchasing: false });
+        //     })
+        //     .catch(error => {
+        //         console.log(error);
+        //         this.setState({ loading: false, purchasing: false });
+        //     });
+        this.props.history.push('/checkout');
+    
     }
 
     render() {
