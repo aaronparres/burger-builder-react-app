1af2f381733ea7624f3623609c36f24c9362e505
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-10-21 18:46:56 +0200

    Fetching orders from server (not printing them yet)

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Axios from '../../axios-orders';
+
+import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler';
+
+export class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        Axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+        );
+    }
+}
+
+export default withErrorHandler(Orders, Axios);
