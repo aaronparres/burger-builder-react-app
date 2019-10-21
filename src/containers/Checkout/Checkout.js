471ceaf9f62e1483b8df54b7e94a8c7eb902743c
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-10-21 17:48:51 +0200

    Pass the price through queryParams aswell (to avoid error when ingredientes is null -> didmount to willmount) , redirect to '/' when submitted

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,24 +6,26 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 
 export class Checkout extends Component {
-    state = { //dummy data to test ingredients
-        ingredients: {
-            salad: 1,
-            bacon: 1,
-            meat: 1,
-            cheese: 1
-        }
+    state = {
+        ingredients: null,
+        totalPrice: 0
     }
 
-    componentDidMount() {
+    UNSAFE_componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()) {
             // console.log(param) // ['salad', '1']
-            ingredients[param[0]] = +param[1]; // Unary Operator By adding a + sign before a String, it will be converted into a number
-            // console.log(ingredients);
+            if (param[0] === 'price') {
+                price = param[1];
+            } else {
+                ingredients[param[0]] = +param[1]; // Unary Operator By adding a + sign before a String, it will be converted into a number
+                // console.log(ingredients);
+            }
+
         }
-        this.setState({ ingredients: ingredients })
+        this.setState({ ingredients: ingredients, totalPrice: price })
     }
 
     checkoutCancelledHandler = () => {
@@ -45,9 +47,9 @@ export class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
                 {/* <Route path={this.props.match.path + '/contact-data'} component={ContactData} /> */}
-                <Route 
+                <Route
                     path={this.props.match.path + '/contact-data'}
-                    render={() => (<ContactData ingredients={this.state.ingredients} />)} />
+                    render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
             </div>
         );
     }
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
@@ -17,11 +18,11 @@ class ContactData extends Component {
 
     orderHandler = (e) => {
         e.preventDefault(); //prevent form submit reloading the page
-    
+
         this.setState({ loading: true });
         const order = {
             ingredients: this.props.ingredients,
-            price: this.state.totalPrice,
+            price: this.props.price,
             customer: {
                 name: 'Aarón Parres',
                 address: {
@@ -37,25 +38,32 @@ class ContactData extends Component {
         Axios.post('/orders.json', order)
             .then(response => {
                 console.log(response);
-                this.setState({ loading: false, /* purchasing: false */ });
+                this.setState({ loading: false });
+                this.props.history.push('/');
             })
             .catch(error => {
                 console.log(error);
-                this.setState({ loading: false, /* purchasing: false */ });
+                this.setState({ loading: false });
             });
     }
 
     render() {
+        let form = (
+            <form>
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clickedButton={this.orderHandler}>ORDER</Button>
+            </form>);
+
+        if (this.state.loading) {
+            form = <Spinner />
+        }
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success" clickedButton={this.orderHandler}>ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
