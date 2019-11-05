9532fae099cbb7162a05eae545dc837ebbb78c83
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-11-05 18:41:20 +0100

    Complete form submission to be stored in Firebase along with the order (builded burger)

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,9 +67,14 @@ class ContactData extends Component {
         e.preventDefault(); //prevent form submit reloading the page
 
         this.setState({ loading: true });
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price
+            price: this.props.price,
+            orderData: formData
         };
 
         Axios.post('/orders.json', order)
@@ -104,16 +109,16 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value} ç
-                        change={(event) => this.inputChangedHandler(event, formElement.id)} />
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
-                <Button btnType="Success" clickedButton={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success">ORDER</Button>
             </form>);
 
         if (this.state.loading) {
