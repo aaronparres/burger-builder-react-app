eb065264798c5c961cab5db24707caaf111e354f
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-11-05 18:26:56 +0100

    inputChangedHandler method to update the values from the inputs --> (deep cloning the form and the desired element to update it immutably)

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -53,8 +53,8 @@ class ContactData extends Component {
                 elementType: 'select',
                 elementConfig: {
                     options: [
-                        {value: 'fastest', displayValue: 'Fastest'},
-                        {value: 'cheapest', displayValue: 'Cheapest'}
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' }
                     ]
                 },
                 value: 'fastest' //default value to avoid empty string ' ' when submitting
@@ -63,57 +63,68 @@ class ContactData extends Component {
         loading: false
     }
 
-orderHandler = (e) => {
-    e.preventDefault(); //prevent form submit reloading the page
+    orderHandler = (e) => {
+        e.preventDefault(); //prevent form submit reloading the page
 
-    this.setState({ loading: true });
-    const order = {
-        ingredients: this.props.ingredients,
-        price: this.props.price
-    };
+        this.setState({ loading: true });
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price
+        };
 
-    Axios.post('/orders.json', order)
-        .then(response => {
-            console.log(response);
-            this.setState({ loading: false });
-            this.props.history.push('/');
-        })
-        .catch(error => {
-            console.log(error);
-            this.setState({ loading: false });
-        });
-}
-
-render() {
-    const formElementsArray = [];
-    for(let key in this.state.orderForm){
-        formElementsArray.push({
-            id: key,
-            config: this.state.orderForm[key]
-        })
+        Axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+                this.setState({ loading: false });
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({ loading: false });
+            });
     }
 
-    let form = (
-        <form>
-            {formElementsArray.map(formElement => (
-                <Input
-                    key={formElement.id} 
-                    elementType={formElement.config.elementType} 
-                    elementConfig={formElement.config.elementConfig} 
-                    value={formElement.config.value} />
-            ))}
-            <Button btnType="Success" clickedButton={this.orderHandler}>ORDER</Button>
-        </form>);
-
-    if (this.state.loading) {
-        form = <Spinner />
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = { ...this.state.orderForm };
+        const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
+        /* To deep clone the inner Object (name or street or etc. depends on the indetifier) 
+        inside the orderForm object which was cloned from the state */
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({ orderForm: updatedOrderForm });
     }
-    return (
-        <div className={classes.ContactData}>
-            <h4>Enter your Contact Data</h4>
-            {form}
-        </div>
-    );
-}
+
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
+        let form = (
+            <form>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} ç
+                        change={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <Button btnType="Success" clickedButton={this.orderHandler}>ORDER</Button>
+            </form>);
+
+        if (this.state.loading) {
+            form = <Spinner />
+        }
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Data</h4>
+                {form}
+            </div>
+        );
+    }
 }
 export default ContactData;
