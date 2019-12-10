f95323c5e1716b6b308aa4ec47eab3e3f579c1ac
Aarón Parres López <aaronplmultimedia@gmail.com>
2019-12-10 21:01:21 +0100

    If we are authenticated (after login submit) it redirects to "/" and the Orders NavItem only appears when logged in

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,11 +4,10 @@ import classes from './NavigationItems.module.css';
 import NavItem from './NavItem/NavItem';
 
 const NavigationItems = (props) => {
-    console.log(props)
     return (
         <ul className={classes.NavigationItems}>
             <NavItem link="/" exact>Burger Builder</NavItem>
-            <NavItem link="/orders">Orders</NavItem>
+            {props.isAuthenticated ? <NavItem link="/orders">Orders</NavItem> : null}
             {!props.isAuthenticated ? <NavItem link="/login">Login</NavItem> : <NavItem link="/logout">Logout</NavItem>}
         </ul>
     );
