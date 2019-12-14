3d62737ace1253edda217cd3277f8ba9f01af654
Aarón Parres <38409133+aaronparres@users.noreply.github.com>
2019-12-14 16:37:33 +0100

    Guarding the routes if not authenticated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './containers/Layout/Layout';
@@ -11,30 +11,52 @@ import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
 class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignin();
-  }
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/orders" component={Orders} />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/login" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" component={BurgerBuilder} />
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        this.props.onTryAtoSignin();
+    }
+
+    render() {
+
+        let routes = (
+            <Switch>
+                <Route path="/login" component={Auth} />
+                <Route path="/" component={BurgerBuilder} />
+                <Redirect to="/" />
+            </Switch>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/logout" component={Logout} />
+                    <Route path="/" component={BurgerBuilder} />
+                    <Redirect to="/" />
+                </Switch>
+            );
+        }
+        return (
+            <div>
+                <Layout>
+                    {routes}
+                </Layout>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    onTryAutoSignin: () => dispatch(actions.authCheckState())
-  }
+    return {
+        onTryAutoSignin: () => dispatch(actions.authCheckState())
+    }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
