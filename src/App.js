5b873d5087b27954c8eac5d52d13bad546f9cdc6
Aarón Parres <38409133+aaronparres@users.noreply.github.com>
2019-12-14 16:39:06 +0100

    Fixed redirect to Checkout from Login page when applying isAuthenticated guard

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import * as actions from './store/actions/index';
 class App extends Component {
 
     componentDidMount() {
-        this.props.onTryAtoSignin();
+        this.props.onTryAutoSignin();
     }
 
     render() {
@@ -32,6 +32,7 @@ class App extends Component {
                     <Route path="/orders" component={Orders} />
                     <Route path="/checkout" component={Checkout} />
                     <Route path="/logout" component={Logout} />
+                    <Route path="/login" component={Auth} />
                     <Route path="/" component={BurgerBuilder} />
                     <Redirect to="/" />
                 </Switch>
